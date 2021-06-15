4e11c573d8e458840dd796e24d89f34b6e4febfa
Deano License <d.license@dectech.co.uk>
2021-06-15 09:57:23 +0100

    Initial Commit

---
+++ Muhtal Test/Muhtal_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muhtal_TestApp.swift
+//  Muhtal Test
+//
+//  Created by Deano License on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Muhtal_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
